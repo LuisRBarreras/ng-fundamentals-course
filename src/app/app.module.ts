3e4b936f08b76f37b642a7a54df62c4b118b9f87
Luis Barreras <luis.barreras@unosquare.com>
2019-03-29 12:12:43 -0700

    feat(route-guards & profile module) routerGuard on deactivate, add resolve rxjs for events, create profile module & organize imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import {
+  EventListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/events-list.component';
-import { EventService } from './events/shared/event.service';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator-service';
 
 @NgModule({
   imports: [
@@ -28,7 +32,20 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     CreateEventComponent,
     Error404Component
   ],
-  providers: [EventService, ToastrService, EventRouteActivator],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  }
+  return true;
+}
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,14 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nav-bar',
   templateUrl: './navbar.component.html',
-  styles: [
-    `
+  styles: [ `
       .nav.navbar-nav {font-size: 15px;}
       #searchForm {margin-right: 100px;}
       @media (max-wdith: 1200px) { #searchForm {display: none}}
-    `
-
-  ]
+      li > a.active { color: #F97924; }
+    `]
 })
 export class NavBarComponent {
 
