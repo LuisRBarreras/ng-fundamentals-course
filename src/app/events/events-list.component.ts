9419df27a975ef7d35c07097c9e5a504cee67234
Luis Barreras <luis.barreras@unosquare.com>
2019-04-08 17:11:58 -0700

    feature(create login & authService) Creation of login interface and authentication service

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
-import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared';
   
 
 @Component({
@@ -18,7 +19,7 @@ import { ActivatedRoute } from '@angular/router';
   `
 })
 export class EventListComponent implements OnInit {
-  events: any;
+  events: IEvent[];
   
   constructor(private eventService: EventService, private toastr: ToastrService,
     private route:ActivatedRoute) {
