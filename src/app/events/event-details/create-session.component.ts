3fbe79c2bf8e7b99ed882bcc624872b83f3e67ce
Luis Barreras <luis.barreras@unosquare.com>
2019-05-03 21:00:01 -0700

    feature(create-session) save new session data

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ISession, restrictedWords } from '../shared/index';
 
 @Component({
+  selector: 'create-session',
   templateUrl: './create-session.component.html',
   styles: [`
   em { float:right; color:#E05C65; padding-left: 10px; }
@@ -14,7 +15,9 @@ import { ISession, restrictedWords } from '../shared/index';
 `]
 })
 export class CreateSessionComponent implements OnInit {
-  newSessionForm: FormGroup
+  @Output() saveNewSession = new EventEmitter()
+  @Output() cancelAddSession = new EventEmitter()
+  newSessionForm: FormGroup;
   name: FormControl
   presenter: FormControl
   duration: FormControl
@@ -47,7 +50,14 @@ export class CreateSessionComponent implements OnInit {
       presenter: formValues.presenter,
       abstract: formValues.abstract,
       voters: []
-    }
+    };
+
+    this.saveNewSession.emit(session);
+    
+  }
+
+  cancel() {
+    this.cancelAddSession.emit();    
   }
 
 }
